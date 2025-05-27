e903aa28834feeb64b8f8969c89ccd0d170dab09
hbq969 <hbq969@gmail.com>
2025-05-27 18:11:09 +0800

    格式化代码

--- dbc-config/src/main/java/com/github/hbq969/middleware/dbc/service/impl/APIServiceImpl.java
+++ dbc-config/src/main/java/com/github/hbq969/middleware/dbc/service/impl/APIServiceImpl.java
@@ -34,7 +34,8 @@ public class APIServiceImpl implements APIService {
         model.setUsername(UserContext.get().getUserName());
 
         List<ConfigEntity> profileConfigs = apiDao.queryConfigList(model);
-        Map<String, ConfigEntity> profileConfigMap = profileConfigs.stream().collect(Collectors.toMap(ConfigEntity::getConfigKey, e -> e, (k1, k2) -> k2));
+        Map<String, ConfigEntity> profileConfigMap = profileConfigs.stream()
+                .collect(Collectors.toMap(ConfigEntity::getConfigKey, e -> e, (k1, k2) -> k2));
 
         model.setProfileName("default");
         List<ConfigEntity> defaultConfigs = apiDao.queryConfigList(model);
@@ -46,21 +47,28 @@ public class APIServiceImpl implements APIService {
                 ConfigEntity profileConfig = profileConfigMap.remove(e.getConfigKey());
                 TypePair pair;
                 if (profileConfig != null) {
-                    pair = new TypePair(profileConfig.getConfigKey(), profileConfig.getConfigValue(), profileConfig.getDataType());
+                    pair = new TypePair(profileConfig.getConfigKey(),
+                            profileConfig.getConfigValue(),
+                            profileConfig.getDataType());
                 } else {
-                    pair = new TypePair(e.getConfigKey(), e.getConfigValue(), e.getDataType());
+                    pair = new TypePair(e.getConfigKey(),
+                            e.getConfigValue(),
+                            e.getDataType());
                 }
                 return pair;
             }).collect(Collectors.toList());
             // 增加追加profile多出来的
             if(MapUtil.isNotEmpty(profileConfigMap)){
                 for (ConfigEntity conf : profileConfigMap.values()) {
-                    pairs.add(new TypePair(conf.getConfigKey(), conf.getConfigValue(), conf.getDataType()));
+                    pairs.add(new TypePair(conf.getConfigKey(),
+                            conf.getConfigValue(),
+                            conf.getDataType()));
                 }
             }
         } else {
-            pairs = profileConfigs.stream().map(e -> new TypePair(e.getConfigKey(), e.getConfigValue(), e.getDataType())).
-                    collect(Collectors.toList());
+            pairs = profileConfigs.stream()
+                    .map(e -> new TypePair(e.getConfigKey(), e.getConfigValue(), e.getDataType()))
+                    .collect(Collectors.toList());
         }
 
         if (model.isProp()) {
